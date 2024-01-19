66e700326fac9f158065f7f580e0a5bc6f34d9d8
Yuri-stack <yurioliveirasilva1999l@gmail.com>
2024-01-19 12:37:31 -0300

    Aula de Integração com o Back

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import Home from "./pages/Home/Home"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+
+import Login from './pages/Login/Login'
+import Cadastro from './pages/Cadastro/Cadastro'
+import Home from './pages/Home/Home'
 
 function App() {
-  return (
-    <Home />
-  )
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Navbar />
+                <div className='min-h-[80vh]'>
+                    <Routes>
+                    <Route path="/" element={<Login />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/cadastro" element={<Cadastro />} />
+                        <Route path="/home" element={<Home />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
+        </ AuthProvider>
+    )
 }
 
 export default App
