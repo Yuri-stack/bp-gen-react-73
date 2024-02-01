30f7c28c8105fa995192fba09cce328c53f43bfd
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-02-01 10:11:40 -0300

    Renderização Condicional

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -32,6 +32,18 @@ function Navbar() {
                 </div>
             </div>
         )
+    } else {
+        navbarComponent = (
+            <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+                <div className="container flex justify-between text-lg">
+                    <Link to='' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
+
+                    <div className='flex gap-4'>
+                        <Link to='/cadastro'>Cadastre-se</Link>
+                    </div>
+                </div>
+            </div>
+        )
     }
 
     return (
